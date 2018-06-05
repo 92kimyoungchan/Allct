b09da8cb00871d2c295cc260bf8506d2ee7d9764
Young <Young@young>
2018-06-05 17:51:33 +0900

    회원정보입력 수정중

--- RealALL/WebContent/assets/js/datepicker.js
+++ RealALL/WebContent/assets/js/datepicker.js
@@ -1,7 +1,7 @@
 	$(document).ready(function() {
 
 		   function datepicker(){
-			      $('.datepicker').datepicker({
+			      $('.datepicker_first').datepicker({
 			         dateFormat: "yy-mm-dd",
 			         regional: "ko",
 			         mode :"multiple",
@@ -10,6 +10,16 @@
 			         dayNamesShort : [ "일", "월", "화", "수", "목", "금", "토" ] ,
 			         monthNames : [ "1월", "2월", "3월", "4월", "5월", "6월",  "7월", "8월", "9월", "10월", "11월", "12월" ]
 			      });
+			      
+			      $('.datepicker_last').datepicker({
+				         dateFormat: "yy-mm-dd",
+				         regional: "ko",
+				         mode :"multiple",
+				         dayNames : [ "일요일", "월요일", "화요일", "수요일",  "목요일", "금요일", "토요일" ],
+				         dayNamesMin : ["일", "월", "화", "수", "목", "금", "토" ],
+				         dayNamesShort : [ "일", "월", "화", "수", "목", "금", "토" ] ,
+				         monthNames : [ "1월", "2월", "3월", "4월", "5월", "6월",  "7월", "8월", "9월", "10월", "11월", "12월" ]
+				      });
 			   }
 			   
 			   $(function() {
